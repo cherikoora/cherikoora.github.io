7c6e47eb13e2dfe409e4d743f06edd68f6473374
cherikoora <diana.kvas124@gmail.com>
2026-04-14 00:17:05 +0500

    обновила карточки.

--- js/script.js
+++ js/script.js
@@ -9,3 +9,28 @@ fetch('footer.html')
     .then(data => {
         document.getElementById('footer-placeholder').innerHTML = data;
     });
+fetch("posts/post-1.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('post-1-placeholder').innerHTML = data;
+    });
+fetch("posts/post-2.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('post-2-placeholder').innerHTML = data;
+    });
+fetch("posts/post-3.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('post-3-placeholder').innerHTML = data;
+    });
+fetch("posts/post-4.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('post-4-placeholder').innerHTML = data;
+    });
+fetch("posts/post-5.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('post-5-placeholder').innerHTML = data;
+    });
